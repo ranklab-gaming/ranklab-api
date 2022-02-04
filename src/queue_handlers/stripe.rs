8358279909a3957912cf722d708f67c87d4aeb72
Matteo Depalo <matteo@depalo.me>
2022-02-04 10:08:14 +0000

    WIP: two handlers

--- src/queue_handlers/stripe.rs
+++ src/queue_handlers/stripe.rs
@@ -18,118 +18,21 @@ struct SqsMessageBody {
   headers: Headers,
 }
 
-pub struct StripeHandler {
-  db_conn: DbConn,
-  config: Config,
-}
-
 #[async_trait]
-impl QueueHandler for StripeHandler {
-  fn new(db_conn: DbConn, config: Config) -> Self {
-    Self { db_conn, config }
-  }
+trait StripeEventHandler {
+  fn new(db_conn: DbConn, config: Config) -> Self;
 
-  fn url(&self) -> String {
-    self.config.stripe_connect_webhooks_queue.clone()
-  }
-
-  async fn handle(
+  async fn handle_event(
     &self,
-    message: &rusoto_sqs::Message,
+    webhook: stripe::WebhookEvent,
     profile: &rocket::figment::Profile,
-  ) -> anyhow::Result<QueueHandlerOutcome> {
-    let body = message
-      .body
-      .clone()
-      .ok_or(anyhow::anyhow!("No body in message"))?;
-
-    let message_body: SqsMessageBody = serde_json::from_str(&body)?;
-
-    let webhook = stripe::Webhook::construct_event(
-      message_body.body.as_str(),
-      message_body.headers.stripe_signature.as_str(),
-      self.config.stripe_connect_webhooks_secret.as_str(),
-    )?;
-
-    let livemode = match serde_json::from_str::<serde_json::Value>(message_body.body.as_str()) {
-      Ok(event) => match event["livemode"].as_bool() {
-        Some(livemode) => livemode,
-        None => return Err(anyhow::anyhow!("Livemode is not present").into()),
-      },
-      Err(_) => return Err(anyhow::anyhow!("Could not parse message body").into()),
-    };
-
-    if profile == rocket::Config::RELEASE_PROFILE && !livemode {
-      return Err(anyhow::anyhow!("Received webhook in test mode").into());
-    }
-
-    match webhook.event_type {
-      stripe::EventType::AccountUpdated => self.handle_account_updated(&webhook, profile).await?,
-      stripe::EventType::CheckoutSessionCompleted => {
-        self
-          .handle_checkout_session_completed(&webhook, profile)
-          .await?
-      }
-      _ => (),
-    }
-
-    Ok(QueueHandlerOutcome::Success)
-  }
+  ) -> anyhow::Result<()>;
 }
 
-impl StripeHandler {
-  async fn handle_account_updated(
-    &self,
-    webhook: &stripe::WebhookEvent,
-    profile: &rocket::figment::Profile,
-  ) -> anyhow::Result<()> {
-    use crate::schema::coaches::dsl::*;
+struct Direct;
+struct Connect;
 
-    let account = match &webhook.data.object {
-      stripe::EventObject::Account(account) => account,
-      _ => return Ok(()),
-    };
-
-    let details_submitted = match &account.details_submitted {
-      Some(details_submitted) => **details_submitted,
-      None => false,
-    };
-
-    let payouts_enabled = match &account.payouts_enabled {
-      Some(payouts_enabled) => **payouts_enabled,
-      None => false,
-    };
-
-    let account_id = account.id.clone();
-
-    let result = self
-      .db_conn
-      .run::<_, diesel::result::QueryResult<_>>(move |conn| {
-        let existing_coach = coaches.filter(stripe_account_id.eq(account_id.to_string()));
-
-        diesel::update(existing_coach)
-          .set((
-            stripe_payouts_enabled.eq(payouts_enabled),
-            stripe_details_submitted.eq(details_submitted),
-          ))
-          .execute(conn)?;
-
-        Ok(())
-      })
-      .await;
-
-    if let Err(diesel::result::Error::NotFound) = result {
-      if profile == rocket::Config::RELEASE_PROFILE {
-        return Err(diesel::result::Error::NotFound.into());
-      } else {
-        return Ok(());
-      }
-    }
-
-    result?;
-    Ok(())
-  }
-
+impl Direct {
   async fn handle_checkout_session_completed(
     &self,
     webhook: &stripe::WebhookEvent,
@@ -188,5 +91,160 @@ impl StripeHandler {
     result?;
 
     Ok(())
+  }
+}
+
+impl Connect {
+  async fn handle_account_updated(
+    &self,
+    webhook: &stripe::WebhookEvent,
+    profile: &rocket::figment::Profile,
+  ) -> anyhow::Result<()> {
+    use crate::schema::coaches::dsl::*;
+
+    let account = match &webhook.data.object {
+      stripe::EventObject::Account(account) => account,
+      _ => return Ok(()),
+    };
+
+    let details_submitted = match &account.details_submitted {
+      Some(details_submitted) => **details_submitted,
+      None => false,
+    };
+
+    let payouts_enabled = match &account.payouts_enabled {
+      Some(payouts_enabled) => **payouts_enabled,
+      None => false,
+    };
+
+    let account_id = account.id.clone();
+
+    let result = self
+      .db_conn
+      .run::<_, diesel::result::QueryResult<_>>(move |conn| {
+        let existing_coach = coaches.filter(stripe_account_id.eq(account_id.to_string()));
+
+        diesel::update(existing_coach)
+          .set((
+            stripe_payouts_enabled.eq(payouts_enabled),
+            stripe_details_submitted.eq(details_submitted),
+          ))
+          .execute(conn)?;
+
+        Ok(())
+      })
+      .await;
+
+    if let Err(diesel::result::Error::NotFound) = result {
+      if profile == rocket::Config::RELEASE_PROFILE {
+        return Err(diesel::result::Error::NotFound.into());
+      } else {
+        return Ok(());
+      }
+    }
+
+    result?;
+    Ok(())
+  }
+}
+
+#[async_trait]
+impl StripeEventHandler for Direct {
+  fn new(db_conn: DbConn, config: Config) -> Self {
+    Self { db_conn, config }
+  }
+
+  async fn handle_event(
+    &self,
+    webhook: stripe::WebhookEvent,
+    profile: &rocket::figment::Profile,
+  ) -> anyhow::Result<()> {
+    match webhook.event_type {
+      stripe::EventType::CheckoutSessionCompleted => {
+        self
+          .handle_checkout_session_completed(&webhook, profile)
+          .await?
+      }
+      _ => (),
+    }
+
+    Ok(())
+  }
+}
+
+#[async_trait]
+impl StripeEventHandler for Connect {
+  fn new(db_conn: DbConn, config: Config) -> Self {
+    Self { db_conn, config }
+  }
+
+  async fn handle_event(
+    &self,
+    webhook: stripe::WebhookEvent,
+    profile: &rocket::figment::Profile,
+  ) -> anyhow::Result<()> {
+    match webhook.event_type {
+      stripe::EventType::AccountUpdated => self.handle_account_updated(&webhook, profile).await?,
+      _ => (),
+    }
+
+    Ok(())
+  }
+}
+
+pub struct StripeHandler<T: StripeEventHandler> {
+  db_conn: DbConn,
+  config: Config,
+  handler: T,
+}
+
+#[async_trait]
+impl<T: StripeEventHandler + Sync + Send> QueueHandler for StripeHandler<T> {
+  fn new(db_conn: DbConn, config: Config) -> Self {
+    Self {
+      db_conn,
+      config,
+      handler: T::new(),
+    }
+  }
+
+  fn url(&self) -> String {
+    self.config.stripe_connect_webhooks_queue.clone()
+  }
+
+  async fn handle(
+    &self,
+    message: &rusoto_sqs::Message,
+    profile: &rocket::figment::Profile,
+  ) -> anyhow::Result<QueueHandlerOutcome> {
+    let body = message
+      .body
+      .clone()
+      .ok_or(anyhow::anyhow!("No body in message"))?;
+
+    let message_body: SqsMessageBody = serde_json::from_str(&body)?;
+
+    let webhook = stripe::Webhook::construct_event(
+      message_body.body.as_str(),
+      message_body.headers.stripe_signature.as_str(),
+      self.config.stripe_connect_webhooks_secret.as_str(),
+    )?;
+
+    let livemode = match serde_json::from_str::<serde_json::Value>(message_body.body.as_str()) {
+      Ok(event) => match event["livemode"].as_bool() {
+        Some(livemode) => livemode,
+        None => return Err(anyhow::anyhow!("Livemode is not present").into()),
+      },
+      Err(_) => return Err(anyhow::anyhow!("Could not parse message body").into()),
+    };
+
+    if profile == rocket::Config::RELEASE_PROFILE && !livemode {
+      return Err(anyhow::anyhow!("Received webhook in test mode").into());
+    }
+
+    match self.handler.handle_event(webhook).await {
+      Ok(_) => Ok(QueueHandlerOutcome::Success),
+      Err(e) => Err(e),
+    }
   }
 }
