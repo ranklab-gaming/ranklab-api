1c4d45276a773353f62cf21507464da82ef2fa69
Eugenio Depalo <eugeniodepalo@gmail.com>
2022-02-16 10:13:13 +0000

    Remove card method type

--- src/routes/player/stripe_payment_intents.rs
+++ src/routes/player/stripe_payment_intents.rs
@@ -80,7 +80,6 @@ pub async fn create(
 
   params.customer = Some(customer_id);
   params.description = Some("Recording payment");
-  params.payment_method_types = Some(vec!["card".to_string()].into());
   params.setup_future_usage = Some(stripe::PaymentIntentSetupFutureUsage::OnSession);
 
   let payment_intent = stripe::PaymentIntent::create(&stripe.0 .0, params)
