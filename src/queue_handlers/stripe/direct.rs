8b54c009442ff85419981f581f0767ba69d9fd0d
Matteo Depalo <matteo@depalo.me>
2022-02-17 16:43:20 +0000

    Implement handle_payment_intent_succeeded and refund

--- src/queue_handlers/stripe/direct.rs
+++ src/queue_handlers/stripe/direct.rs
@@ -1,16 +1,112 @@
 use super::StripeEventHandler;
+use crate::data_types::ReviewState;
+use crate::emails::{Email, Recipient};
+use crate::models::{Coach, Review};
 use crate::{config::Config, guards::DbConn};
+use diesel::prelude::*;
+use serde_json::json;
+use stripe::Expandable;
 
 pub struct Direct {
   config: Config,
+  db_conn: DbConn,
 }
 
-impl Direct {}
+impl Direct {
+  async fn handle_payment_intent_succeeded(
+    &self,
+    webhook: stripe::WebhookEvent,
+  ) -> anyhow::Result<()> {
+    let payment_intent_id = match &webhook.data.object {
+      stripe::EventObject::PaymentIntent(payment_intent) => payment_intent.id.clone(),
+      _ => return Ok(()),
+    };
+
+    self
+      .db_conn
+      .run(move |conn| {
+        use crate::schema::reviews::dsl::{reviews, state, stripe_payment_intent_id};
+
+        diesel::update(reviews.filter(stripe_payment_intent_id.eq(payment_intent_id.to_string())))
+          .set(state.eq(ReviewState::AwaitingReview))
+          .get_result::<Review>(conn)
+          .unwrap()
+      })
+      .await;
+
+    let coaches = self
+      .db_conn
+      .run(move |conn| {
+        use crate::schema::coaches::dsl::*;
+
+        coaches.load::<Coach>(conn).unwrap()
+      })
+      .await;
+
+    let email = Email::new(
+      &self.config,
+      "notification".to_owned(),
+      json!({
+          "subject": "New VODs are available",
+          "title": "There are new VODs available for review!",
+          "body": "Go to your dashboard to start analyzing them.",
+          "cta" : "View Available VODs",
+          "cta_url" : "https://ranklab.gg/dashboard"
+      }),
+      coaches
+        .iter()
+        .map(|coach| {
+          Recipient::new(
+            coach.email.clone(),
+            json!({
+              "name": coach.name.clone(),
+            }),
+          )
+        })
+        .collect(),
+    );
+
+    email.deliver();
+
+    Ok(())
+  }
+
+  async fn handle_charge_refunded(&self, webhook: stripe::WebhookEvent) -> anyhow::Result<()> {
+    let charge = match &webhook.data.object {
+      stripe::EventObject::Charge(charge) => charge,
+      _ => return Ok(()),
+    };
+
+    if !charge.refunded {
+      return Ok(());
+    }
+
+    let payment_intent_id = match charge.payment_intent.clone() {
+      Some(box Expandable::Id(payment_intent_id)) => payment_intent_id,
+      Some(box Expandable::Object(payment_intent)) => payment_intent.id,
+      None => return Err(anyhow::anyhow!("No payment intent found")),
+    };
+
+    self
+      .db_conn
+      .run(move |conn| {
+        use crate::schema::reviews::dsl::{reviews, state, stripe_payment_intent_id};
+
+        diesel::update(reviews.filter(stripe_payment_intent_id.eq(payment_intent_id.to_string())))
+          .set(state.eq(ReviewState::Refunded))
+          .get_result::<Review>(conn)
+          .unwrap()
+      })
+      .await;
+
+    Ok(())
+  }
+}
 
 #[async_trait]
 impl StripeEventHandler for Direct {
-  fn new(_db_conn: DbConn, config: Config) -> Self {
-    Self { config }
+  fn new(db_conn: DbConn, config: Config) -> Self {
+    Self { config, db_conn }
   }
 
   fn url(&self) -> String {
@@ -27,85 +123,12 @@ impl StripeEventHandler for Direct {
     _profile: &rocket::figment::Profile,
   ) -> anyhow::Result<()> {
     match webhook.event_type {
+      stripe::EventType::PaymentIntentSucceeded => {
+        self.handle_payment_intent_succeeded(webhook).await?
+      }
+      stripe::EventType::ChargeRefunded => self.handle_charge_refunded(webhook).await?,
       _ => (),
     }
-
-    //   let recording_id = review.recording_id.clone();
-
-    // let recording = db_conn
-    //   .run(move |conn| {
-    //     use crate::schema::recordings::dsl::{id, recordings};
-    //     recordings
-    //       .filter(id.eq(recording_id))
-    //       .first::<Recording>(conn)
-    //   })
-    //   .await?;
-
-    // if recording.stripe_payment_intent_id.is_none() {
-    //   return Response::mutation_error(Status::BadRequest);
-    // }
-
-    // db_conn
-    //   .run(move |conn| {
-    //     use crate::schema::recordings::dsl::*;
-    //     diesel::update(crate::schema::recordings::table.find(recording_id))
-    //       .set((stripe_payment_intent_id.eq::<Option<String>>(None),))
-    //       .execute(conn)
-    //       .unwrap();
-    //   })
-    //   .await;
-
-    // let review: ReviewView = db_conn
-    //   .run(move |conn| {
-    //     use crate::schema::reviews::dsl::*;
-
-    //     diesel::insert_into(reviews)
-    //       .values((
-    //         recording_id.eq(review.recording_id.clone()),
-    //         title.eq(review.title.clone()),
-    //         game_id.eq(review.game_id.clone()),
-    //         player_id.eq(auth.0.id.clone()),
-    //         skill_level.eq(game.unwrap().skill_level as i16),
-    //         notes.eq(review.notes.clone()),
-    //         stripe_payment_intent_id.eq(recording.stripe_payment_intent_id.unwrap()),
-    //       ))
-    //       .get_result::<Review>(conn)
-    //       .unwrap()
-    //   })
-    //   .await
-    //   .into();
-
-    // let coaches = db_conn
-    //   .run(move |conn| {
-    //     use crate::schema::coaches::dsl::*;
-    //     coaches.load::<Coach>(conn).unwrap()
-    //   })
-    //   .await;
-
-    // let email = Email::new(
-    //   config,
-    //   "notification".to_owned(),
-    //   json!({
-    //       "subject": "New VODs are available",
-    //       "title": "There are new VODs available for review!",
-    //       "body": "Go to your dashboard to start analyzing them.",
-    //       "cta" : "View Available VODs",
-    //       "cta_url" : "https://ranklab.gg/dashboard"
-    //   }),
-    //   coaches
-    //     .iter()
-    //     .map(|coach| {
-    //       Recipient::new(
-    //         coach.email.clone(),
-    //         json!({
-    //           "name": coach.name.clone(),
-    //         }),
-    //       )
-    //     })
-    //     .collect(),
-    // );
-
-    // email.deliver();
 
     Ok(())
   }
