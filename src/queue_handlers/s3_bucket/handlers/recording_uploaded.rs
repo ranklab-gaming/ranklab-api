ef0e82ca171aa80b62e020934d33b099b75f8e4a
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-07-26 09:59:50 +0000

    Send email when VOD is processed

--- src/queue_handlers/s3_bucket/handlers/recording_uploaded.rs
+++ src/queue_handlers/s3_bucket/handlers/recording_uploaded.rs
@@ -1,9 +1,11 @@
 use crate::data_types::MediaState;
+use crate::emails::{Email, Recipient};
 use crate::fairings::sqs::QueueHandlerError;
-use crate::models::{Recording, RecordingChangeset};
+use crate::models::{Recording, RecordingChangeset, RecordingMetadata, User};
 use crate::queue_handlers::s3_bucket::Record;
 use crate::queue_handlers::S3BucketHandler;
 use diesel::prelude::*;
+use serde_json::json;
 
 pub async fn handle_recording_uploaded(
   handler: &S3BucketHandler,
@@ -40,10 +42,21 @@ pub async fn handle_recording_uploaded(
   if file.ends_with(".mp4") {
     let processed_video_key = Some(record.s3.object.key.clone());
 
+    let recording: Recording = handler
+      .db_conn
+      .run(move |conn| recording_query.first::<Recording>(conn))
+      .await
+      .map_err(QueueHandlerError::from)?;
+
+    let metadata = recording.metadata.clone();
+    let recording_id = recording.id;
+    let user_id = recording.user_id;
+    let title = recording.title.clone();
+
     handler
       .db_conn
       .run::<_, diesel::result::QueryResult<_>>(move |conn| {
-        diesel::update(recording_query)
+        diesel::update(&recording)
           .set(
             RecordingChangeset::default()
               .state(MediaState::Processed)
@@ -53,6 +66,41 @@ pub async fn handle_recording_uploaded(
       })
       .await
       .map_err(QueueHandlerError::from)?;
+
+    if let Some(metadata) = metadata {
+      let parsed_metadata = serde_json::from_value::<RecordingMetadata>(metadata)
+        .map_err(|e| anyhow::anyhow!("Failed to parse metadata: {}", e))?;
+
+      let user = handler
+        .db_conn
+        .run(move |conn| User::find_by_id(&user_id).first::<User>(conn))
+        .await?;
+
+      if parsed_metadata.is_overwatch() {
+        let video_uploaded = Email::new(
+          &handler.config,
+          "notification".to_owned(),
+          json!({
+            "subject": "Your VOD has been processed",
+            "title": format!("The VOD for \"{}\" has been processed", title),
+            "body": "You can follow the link below to view it.",
+            "cta" : "View VOD",
+            "cta_url" : format!("{}/recordings/{}", handler.config.web_host, recording_id),
+          }),
+          vec![Recipient::new(
+            user.email,
+            json!({
+              "name": user.name,
+            }),
+          )],
+        );
+
+        video_uploaded
+          .deliver()
+          .await
+          .map_err(|e| anyhow::anyhow!("Failed to send email: {}", e))?;
+      }
+    }
 
     return Ok(());
   }
