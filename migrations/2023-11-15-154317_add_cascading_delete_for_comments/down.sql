eaa56156b5245e518b74395df114645d9bcef186
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-11-15 15:47:23 +0000

    Delete comments on cascade

---
+++ migrations/2023-11-15-154317_add_cascading_delete_for_comments/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE comments DROP CONSTRAINT comments_recording_id_fkey;
+ALTER TABLE comments ADD CONSTRAINT comments_recording_id_fkey FOREIGN KEY (recording_id) REFERENCES recordings(id);
